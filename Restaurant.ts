f372fcfb7e9dd77b9dc58908934819b2ad70613d
KOEV.SONG <KOEV.SONG@pnc.lan>
2022-06-27 07:44:46 +0700

    add attribute3

---
+++ Restaurant.ts
@@ -0,0 +1,12 @@
+import { Order } from "./Order/Order";
+import { Payment } from "./Peyment/Payment";
+import { Person } from "./Staff/Person";
+import { Table } from "./Table/Table";
+
+class Restaurant{
+    payment:Payment;
+    person:Person;
+    order:Order;
+    table:Table;
+
+}
--- Table/Table.ts
+++ Table/Table.ts
@@ -1,4 +1,4 @@
-class Table {
+export class Table {
     constructor(
         private tableNumber: number,
         private tablestatus: boolean,
